58cfc62f0357eb753826d5cfe0f62ac9491555b4
Pawel Klapuch <pawelklapuch@gmail.com>
2024-10-14 12:58:35 -0500

    Preparing tests

--- demo/app.py
+++ demo/app.py
@@ -1,6 +1,8 @@
 import json
 
 def lambda_handler(event, context):
+    test = 200
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
---
+++ tests/unit/test_demo.py
@@ -0,0 +1,11 @@
+# import boto3
+# import moto
+import pytest
+
+# @pytest.fixture(scope='function')
+# def mock():
+#     print("set up mocks!")
+
+def test_demo():
+    assert 2 == 2
+
