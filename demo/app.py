3ce02c1e6c0a0203461e2a0a4dae8993a9a1ddae
Pawel Klapuch <pawelklapuch@gmail.com>
2024-10-14 20:18:34 -0500

    First working unit test.

--- demo/app.py
+++ demo/app.py
@@ -1,12 +1,38 @@
 import json
+import os
+import boto3
+import boto3.dynamodb.conditions as conditions
+
+ENV_TABLE_NAME = "TABLE_NAME"
+
+def get_table_resource():
+    dynamodb_resource = boto3.resource("dynamodb")
+    table_name = os.environ[ENV_TABLE_NAME]
+    return dynamodb_resource.Table(table_name)
+
+def get_fillUps_for_user(user_id: str) -> list:
+    print("TABLE NAME: " + os.environ[ENV_TABLE_NAME])
+    table = get_table_resource()
+
+    response = table.query(
+        KeyConditionExpression=\
+            conditions.Key("userID").eq(f"{user_id}")
+    )
+    
+    return response["Items"]
 
 def lambda_handler(event, context):
-    test = 200
+    response = {}
+    items = []
     
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    try:
+        items = get_fillUps_for_user("1")
+    except Exception as error:
+        print(f"Error: {repr(error)}")
+    except:
+        print("Unknown error")
+    
+    response['statusCode'] = 200
+    response['items'] = items
+    
+    return response
--- tests/unit/test_demo.py
+++ tests/unit/test_demo.py
@@ -1,11 +1,49 @@
-# import boto3
-# import moto
 import pytest
+import boto3
+import os
+from moto import mock_aws
+from demo.app import lambda_handler
+from demo.app import ENV_TABLE_NAME
 
-# @pytest.fixture(scope='function')
-# def mock():
-#     print("set up mocks!")
+TABLE_NAME = "FillUp"
 
-def test_demo():
-    assert 2 == 2
+@pytest.fixture
+def lambda_environment():
+    os.environ[ENV_TABLE_NAME] = TABLE_NAME
 
+def mock_database(item):
+    dynamodb_resource = boto3.resource("dynamodb")
+    table = dynamodb_resource.create_table(
+        TableName=TABLE_NAME,
+        KeySchema=[
+            {"AttributeName": "userID", "KeyType": "HASH"},
+            {"AttributeName": "SK", "KeyType": "RANGE"}
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "userID", "AttributeType": "S"},
+            {"AttributeName": "SK", "AttributeType": "S"}
+        ],
+        ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+    )
+    table.put_item(Item=item)
+
+@mock_aws
+def test_lambda_handler(lambda_environment):
+    """On successful execution returns all stored items"""
+    
+    storedItem = {
+        "userID": "1",
+        "SK": "test",
+        "fillup": "test",
+    }
+
+    mock_database(storedItem)
+
+    response = lambda_handler({}, {})
+    print(response)
+    
+    statusCode = response['statusCode']
+    assert statusCode == 200
+
+    items = response['items']
+    assert items == [storedItem]
